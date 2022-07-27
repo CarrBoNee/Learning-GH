999138117410d408224a5de13db73a7a8c85bc03
julian erlandson <julianerlandson@gmail.com>
2022-07-27 15:19:53 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Julian Erlandsson on 2022-07-27.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
